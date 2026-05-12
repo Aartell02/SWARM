d0dac2a8f359b8fa72f65e4fddcc975782113360
Artur <artur.kuzel@gmail.com>
2026-05-12 10:32:50 +0200

    clr

--- SWARM/include/Transform.hpp
+++ SWARM/include/Transform.hpp
@@ -5,7 +5,7 @@ using namespace DirectX;
 
 struct Transform {
 	XMFLOAT3 position{ 0.0f, 0.0f, 0.0f };
-	XMFLOAT3 rotation{ 0.0f, 0.0f, 0.0f }; // Euler angles in radians
+	XMFLOAT3 rotation{ 0.0f, 0.0f, 0.0f };
 	XMFLOAT3 scale{ 1.0f, 1.0f, 1.0f };
 
 	// Get world matrix from transform
